f42d875a3e0a67524c7545aaf64664129dec901f
Dmitry A <adg@tomsksoft.com>
2023-04-10 10:36:16 +0700

    update exercises

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,10 +13,14 @@ private val RIGHT_HAND_CAPITAL_LETTERS_RUS = RIGHT_HAND_LETTERS_RUS.filter { it 
 private val LEFT_HAND_CAPITAL_LETTERS_RUS = LEFT_HAND_LETTERS_RUS.filter { it != 'ь' && it != 'ъ' }.uppercase()
 
 private const val ALPHABET_RUS = LEFT_HAND_LETTERS_RUS + RIGHT_HAND_LETTERS_RUS
-private const val ONE_HUNDRED_RUS =
-    "и в не на я быть он с что а по это она этот к но они мы как из у который то за свой что весь год от так о для ты же все тот мочь вы человек такой его сказать только или ещё бы себя один как уже до время если сам когда другой вот говорить наш мой знать стать при чтобы дело жизнь кто первый очень два день её новый рука даже во со раз где там под можно ну какой после их работа без самый потом надо хотеть ли слово идти большой должен место иметь ничто"
-private const val ONE_HUNDRED_EN =
-    "the be to of and a in that have I it for not on with he as you do at this but his by from they we say her she or an will my one all would there their what so up out if about who get which go me when make can like time no just him know take person into year your good some could them see other than then now look only come its over think also back after use two how our work first well way even new want because any these give day most us"
+private val ONE_HUNDRED_RUS =
+    "и в не на я быть он с что а по это она этот к но они мы как из у который то за свой что весь год от так о для ты же все тот мочь вы человек такой его сказать только или ещё бы себя один как уже до время если сам когда другой вот говорить наш мой знать стать при чтобы дело жизнь кто первый очень два день её новый рука даже во со раз где там под можно ну какой после их работа без самый потом надо хотеть ли слово идти большой должен место иметь ничто".split(
+        " "
+    )
+private val ONE_HUNDRED_EN =
+    "the be to of and a in that have I it for not on with he as you do at this but his by from they we say her she or an will my one all would there their what so up out if about who get which go me when make can like time no just him know take person into year your good some could them see other than then now look only come its over think also back after use two how our work first well way even new want because any these give day most us".split(
+        " "
+    )
 private const val RIGHT_HAND_LETTERS_EN = "yuiophjklnm"
 private const val LEFT_HAND_LETTERS_EN = "qwertasdfgzxcvb"
 private const val ALPHABET_EN = LEFT_HAND_LETTERS_EN + RIGHT_HAND_LETTERS_EN
@@ -74,6 +78,26 @@ fun getDigitsExercises(): List<String> {
             getAbracadabraWithPrefixAndPostfix("1234567890", length = 200)
 }
 
+//fun List<String>.toText2(length: String): String {
+//    val text = ""
+//
+//    this.forEach {
+//        text += it
+//    }
+//    return text
+//}
+
+fun List<String>.shuffledText(length: Int = 100): String {
+    return this
+        .shuffled()
+        .take(22)
+        .joinToString(" ")
+}
+
+fun String.withPrefix(prefix: String) = "$prefix$this"
+
+fun String.withPostfix(postfix: String) = "$this$postfix"
+
 fun getShuffledWords(text: String): String {
     return text
         .split(" ")
@@ -99,28 +123,22 @@ fun main() {
 
 
     // Русский язык
-    exerciseListRus.add(getShuffledWords(ONE_HUNDRED_RUS))
-    exerciseListRus.add(getShuffledWords(ONE_HUNDRED_RUS))
-    exerciseListRus.add(getAbracadabraWithPrefixAndPostfix(RIGHT_HAND_CAPITAL_LETTERS_RUS))
-    exerciseListRus.add(getAbracadabraWithPrefixAndPostfix(LEFT_HAND_CAPITAL_LETTERS_RUS))
-    exerciseListRus.add(getAbracadabraWithPrefixAndPostfix(LEFT_HAND_CAPITAL_LETTERS_RUS + RIGHT_HAND_CAPITAL_LETTERS_RUS))
-    exerciseListRus.add(getCapitalLettersExercise(ONE_HUNDRED_RUS))
-    exerciseListRus.add(getCapitalLettersExercise(ONE_HUNDRED_RUS))
+    exerciseListRus.add(ONE_HUNDRED_RUS.shuffledText())
+    exerciseListRus.add(getAbracadabraWithPrefixAndPostfix("("))
+    exerciseListRus.add(getAbracadabraWithPrefixAndPostfix(")"))
+    exerciseListRus.add(getAbracadabraWithPrefixAndPostfix("()"))
+    exerciseListRus.add(getShuffledWords(ONE_HUNDRED_RUS.map { it.withPrefix("(") }.map { it.withPostfix(")") }.shuffledText()))
+    exerciseListRus.add(getShuffledWords(ONE_HUNDRED_RUS.map { it.withPrefix("(") }.map { it.withPostfix(")") }.shuffledText()))
+    exerciseListRus.add(getShuffledWords(ONE_HUNDRED_RUS.map { it.withPrefix("(") }.map { it.withPostfix(")") }.shuffledText()))
 
     // English language
-    exerciseListEn.add(getShuffledWords(ONE_HUNDRED_EN))
-    exerciseListEn.add(getShuffledWords(ONE_HUNDRED_EN))
-    exerciseListEn.add(getAbracadabraWithPrefixAndPostfix("."))
-    exerciseListEn.add(getAbracadabraWithPrefixAndPostfix(RIGHT_HAND_LETTERS_EN, postfixes = listOf(". ")))
-    exerciseListEn.add(getAbracadabraWithPrefixAndPostfix(LEFT_HAND_LETTERS_EN, postfixes = listOf(". ")))
-    exerciseListEn.add(getAbracadabraWithPrefixAndPostfix(ALPHABET_EN, postfixes = listOf(". ")))
-    exerciseListEn.add(getAbracadabraWithPrefixAndPostfix(","))
-    exerciseListEn.add(getAbracadabraWithPrefixAndPostfix(RIGHT_HAND_LETTERS_EN, postfixes = listOf(", ")))
-    exerciseListEn.add(getAbracadabraWithPrefixAndPostfix(LEFT_HAND_LETTERS_EN, postfixes = listOf(", ")))
-    exerciseListEn.add(getAbracadabraWithPrefixAndPostfix(ALPHABET_EN, postfixes = listOf(", ")))
-    exerciseListEn.add(getAbracadabraWithPrefixAndPostfix(",."))
-    exerciseListEn.add(getAbracadabraWithPrefixAndPostfix(ALPHABET_EN, postfixes = listOf(", ", ". ")))
-    exerciseListEn.add(getShuffledWords(ONE_HUNDRED_EN.replace(" ", ". ") + ONE_HUNDRED_EN.replace(" ", ", ")))
+    exerciseListEn.add(ONE_HUNDRED_EN.shuffledText())
+    exerciseListEn.add(getAbracadabraWithPrefixAndPostfix(RIGHT_HAND_LETTERS_EN.uppercase()))
+    exerciseListEn.add(getAbracadabraWithPrefixAndPostfix(LEFT_HAND_LETTERS_EN.uppercase()))
+    exerciseListEn.add(getAbracadabraWithPrefixAndPostfix(RIGHT_HAND_LETTERS_EN.uppercase() + LEFT_HAND_LETTERS_EN.uppercase()))
+    exerciseListEn.add(ONE_HUNDRED_EN.map { it.replaceFirstChar { it.uppercase() } }.shuffledText())
+    exerciseListEn.add(ONE_HUNDRED_EN.map { it.replaceFirstChar { it.uppercase() } }.shuffledText())
+    exerciseListEn.add(ONE_HUNDRED_EN.map { it.replaceFirstChar { it.uppercase() } }.shuffledText())
 
 
     println("Русский язык")
